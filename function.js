40fa56606adeaf589647519e58961a0c6eff3b14
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-18 23:55:36 +0200

    new function added

--- function.js
+++ function.js
@@ -1,12 +1,26 @@
-function startFan() {
-    console.log('Stand up');
-    console.log('wlak towards the switch');
-    console.log('Press the switch');
+// function startFan() {
+//     console.log('Stand up');
+//     console.log('wlak towards the switch');
+//     console.log('Press the switch');
+// }
+
+// //call teh function
+// startFan();
+// console.log('Should be go');
+// startFan();
+// console.log('Ready for Lunch');
+// startFan();
+
+// funntion declaration
+function rideCare() {
+    console.log('Registration to website');
+    console.log('Chose your car');
+    console.log('Pay and ride your Destination');
 }
 
-//call teh function
-startFan();
-console.log('Should be go');
-startFan();
-console.log('Ready for Lunch');
-startFan();
+rideCare();
+console.log('Verify from your email');
+rideCare();
+console.log('you can choose different car');
+rideCare();
+console.log('carefully ride');
--- parameter.js
+++ parameter.js
@@ -1,5 +1,11 @@
-function bringFood(money) {
-    console.log('bring some salad')
+// function bringFood(money) {
+//     console.log('bring some salad')
+//     console.log(money);
+// }
+// bringFood(200);
+
+function houseRent(money) {
+    console.log('Ready for new rent')
     console.log(money);
 }
-bringFood(200);
+houseRent(610);
