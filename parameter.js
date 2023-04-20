aecb5294d7cd378409f61d9535af98e1d1eca1a1
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-20 21:33:20 +0200

    new parameter

--- parameter.js
+++ parameter.js
@@ -9,3 +9,9 @@ function houseRent(money) {
     console.log(money);
 }
 houseRent(610);
+
+function main(count) {
+    console.log('Please give me something for count')
+    console.log(count);
+}
+houseRent(1200);
