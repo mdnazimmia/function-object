c72b60266ff7b2fbf22ca41a80b953cb5083135d
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-25 23:58:09 +0200

    new added

--- parameter.js
+++ parameter.js
@@ -15,3 +15,15 @@ function main(count) {
     console.log(count);
 }
 houseRent(1200);
+
+function houseRent(money) {
+    console.log('Ready for new rent')
+    console.log(money);
+}
+houseRent(610);
+
+function main(count) {
+    console.log('Please give me something for count')
+    console.log(count);
+}
+houseRent(1200);
